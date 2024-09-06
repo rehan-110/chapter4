c3f589d6fafbc9071d9f78302129ae88bc1ded28
rehan-110 <ar4084480@gmail.com>
2024-09-06 16:13:22 +0500

    chapter4 files

---
+++ app.js
@@ -0,0 +1,13 @@
+//chapter 4
+
+var home = 100 ,city = "Ratodero ",fullName = "Ahmed Rehan";
+document.write(home + city + fullName)
+
+
+var $rehan = "$Rehan" , $farhan = "_farhan" , _faraz ="faraz" , $faraz = "farazz" , _rehan = "rehan2";
+var firstName ="Ahmed" , lastName = "umar" , fatherName = "abdulHaleem" , city = "Ratoder" , age = 19;
+
+document.write("variables name Only Contain $ _ And Number For Example : $my_1stVariable");
+document.write("Variables must begin with a letter, $ or _ . For example $name, _name or name");
+document.write("Variable names are case sensitive");
+document.write("Variable names should not be JS Keywords")
